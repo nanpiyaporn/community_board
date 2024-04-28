2c177ea36f1c4cd94c2722316fbe5addc5b1ba74
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-04-28 00:11:24 -0400

    Update .gitignore, add robots.txt and manifest.json, update package.json, and refactor App.js, Card.js, Content.js, and Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,20 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
-import Content from "./pages/Content"
+
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <h1>Welcome to Community Board</h1>
+        <h1>Supa Smoothies</h1>
         <Link to="/">Home</Link>
-        <Link to="/create">Create New Topic</Link>
+        <Link to="/create">Create New Smoothie</Link>
       </nav>
-      <br />
-      <div style={{ textAlign: "center" }}>
-       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/:id" element={<Update />} />
-        <Route path="/Content" element={<Content />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,55 +1,7 @@
-import supabase from "../client"
-import { useEffect, useState } from "react"
-import Card from "../components/Card"
-
 const Home = () => {
-  const [fetchError, setFetchError] = useState(null)
-  const [Contentmates,setContentmates] = useState(null)
-  
-  
-  const handleDelete = async (id) => {
-    setContentmates(prevContentmates =>  { 
-      return prevContentmates.filter(Contentmate => Contentmate.id !== id)
-    }
-  )
-  }
-
-  useEffect(() => {
-    const fetcContentmates = async () => {
-        const {data, error } = await supabase
-          .from('board')
-          .select()
-
-        if (error) {
-          setFetchError('Could not fetch content')
-          setContentmates(null)
-          console.error(error)
-          
-        }
-        if(data){
-          setContentmates(data)
-          console.log(data)
-          setFetchError(null)
-        }
-      }
-      fetcContentmates()
-  }, [])
-
-
   return (
     <div className="page home">
-      {fetchError && <p className="fetch-error">{fetchError}</p>}
-      {Contentmates && (
-        <div className="board">
-
-          <div className="board-grid">
-            {Contentmates.map(board => (
-              <Card key ={board.id}board={board} 
-              onDete = {handleDelete} />
-          ))}
-        </div>
-      </div>
-      )}
+      <h2>Home</h2>
     </div>
   )
 }
